0e1a27faf55670b5a899de786bd148c717c27f41
Ajay Ayyappan <ajayab05@gmail.com>
2026-06-07 05:03:28 +0000

    feat(eks): add OIDC provider for IRSA and Karpenter

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -9,3 +9,8 @@ output "cluster_arn" {
 output "cluster_endpoint" {
   value = aws_eks_cluster.this.endpoint
 }
+
+
+output "oidc_provider_arn" {
+  value = aws_iam_openid_connect_provider.eks.arn
+}
