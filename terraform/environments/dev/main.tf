08bf0bc9d9348aaad3289bddb928c959381fc77d
Ajay Ayyappan <ajayab05@gmail.com>
2026-06-07 05:38:44 +0000

    feat(karpenter): create controller IAM role

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -24,3 +24,16 @@ module "eks" {
   private_subnet_ids = module.vpc.private_subnet_ids
   node_role_arn      = module.iam.node_role_arn
 }
+
+
+module "karpenter" {
+  source = "../../modules/karpenter"
+
+  cluster_name     = module.eks.cluster_name
+  cluster_endpoint = module.eks.cluster_endpoint
+
+  oidc_provider_arn = module.eks.oidc_provider_arn
+
+  project     = var.project
+  environment = var.environment
+}
